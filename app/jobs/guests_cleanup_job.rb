3529df12aa094a260c43beed1b086ac0855c077f
rana haseeb <alix@yahoo.com>
2022-07-14 10:14:42 +0500

    usage_active_job

--- app/jobs/guests_cleanup_job.rb
+++ app/jobs/guests_cleanup_job.rb
@@ -1,3 +1,17 @@
 class GuestsCleanupJob < ActiveJob::Base
-queue_as :low_priority
+queue_as :default
+
+  before_enqueue do |job|
+    # do something with the job instance
+  end
+
+  around_perform do |job, block|
+    # do something before perform
+    block.call
+    # do something after perform
+  end
+
+  def perform
+    # Do something later
+  end
 end
